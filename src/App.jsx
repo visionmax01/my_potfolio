bd1854a2275d63a9a738403db4708a44fb56dd66
visionmax01 <121842628+visionmax01@users.noreply.github.com>
2024-08-24 14:20:07 +0530

    1
    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <p>
+    <p >
       welcome to home 
     </p>
   )
